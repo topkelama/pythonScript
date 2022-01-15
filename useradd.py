2bbfc4759573ad1cf94cae75cefaf2a7f4e47af2
Chhiring Lama <chhiring.topkel@gmail.com>
2022-01-15 20:06:07 +0000

    useradd

---
+++ useradd.py
@@ -0,0 +1,25 @@
+#!usr/bin/python3
+
+#this scrip will add a user in the linux machine
+import os
+import subprocess
+import sys
+import getpass
+  
+# add user function
+def add_user():
+  
+     # Ask user to provide the user name
+     username = input("Enter Username ")   
+  
+     # Let user input password
+     password = getpass.getpass()   
+         
+     try:
+         # executing useradd command using subprocess module
+         subprocess.run(['sudo useradd', '-p', password, username ])      
+     except:
+         print(f"Failed to add user.")                     
+         sys.exit(1)
+# involk the function
+add_user()
